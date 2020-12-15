a31bd4123b538aa0ca4dbcb2d56d7c14e5ea0962
MSI\DatVo <you@example.com>
2020-12-15 20:56:08 +0700

    Done

---
+++ GUI/ThemSinhVien.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace AppQLSV.GUI
+{
+    partial class ThemSinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThemSinhVien));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtfirstnam = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.txtpod = new System.Windows.Forms.TextBox();
+            this.rdbMale = new System.Windows.Forms.RadioButton();
+            this.rdbFeMale = new System.Windows.Forms.RadioButton();
+            this.rdbOthersexual = new System.Windows.Forms.RadioButton();
+            this.txtbirthday = new System.Windows.Forms.DateTimePicker();
+            this.cbMalop = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ngày Sinh :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nơi Sinh :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Giới tính :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(341, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Danh sách Lớp :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(517, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtfirstnam
+            // 
+            this.txtfirstnam.Location = new System.Drawing.Point(95, 49);
+            this.txtfirstnam.Name = "txtfirstnam";
+            this.txtfirstnam.Size = new System.Drawing.Size(212, 20);
+            this.txtfirstnam.TabIndex = 0;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(95, 96);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(212, 20);
+            this.txtlastname.TabIndex = 1;
+            // 
+            // txtpod
+            // 
+            this.txtpod.Location = new System.Drawing.Point(410, 96);
+            this.txtpod.Name = "txtpod";
+            this.txtpod.Size = new System.Drawing.Size(182, 20);
+            this.txtpod.TabIndex = 7;
+            // 
+            // rdbMale
+            // 
+            this.rdbMale.AutoSize = true;
+            this.rdbMale.Location = new System.Drawing.Point(133, 156);
+            this.rdbMale.Name = "rdbMale";
+            this.rdbMale.Size = new System.Drawing.Size(47, 17);
+            this.rdbMale.TabIndex = 2;
+            this.rdbMale.TabStop = true;
+            this.rdbMale.Text = "Nam";
+            this.rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbFeMale
+            // 
+            this.rdbFeMale.AutoSize = true;
+            this.rdbFeMale.Location = new System.Drawing.Point(197, 156);
+            this.rdbFeMale.Name = "rdbFeMale";
+            this.rdbFeMale.Size = new System.Drawing.Size(39, 17);
+            this.rdbFeMale.TabIndex = 3;
+            this.rdbFeMale.TabStop = true;
+            this.rdbFeMale.Text = "Nữ";
+            this.rdbFeMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbOthersexual
+            // 
+            this.rdbOthersexual.AutoSize = true;
+            this.rdbOthersexual.Location = new System.Drawing.Point(257, 156);
+            this.rdbOthersexual.Name = "rdbOthersexual";
+            this.rdbOthersexual.Size = new System.Drawing.Size(50, 17);
+            this.rdbOthersexual.TabIndex = 4;
+            this.rdbOthersexual.TabStop = true;
+            this.rdbOthersexual.Text = "Khác";
+            this.rdbOthersexual.UseVisualStyleBackColor = true;
+            // 
+            // txtbirthday
+            // 
+            this.txtbirthday.CustomFormat = "dd/MM/yyyy";
+            this.txtbirthday.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.txtbirthday.Location = new System.Drawing.Point(409, 46);
+            this.txtbirthday.Name = "txtbirthday";
+            this.txtbirthday.Size = new System.Drawing.Size(183, 20);
+            this.txtbirthday.TabIndex = 5;
+            // 
+            // cbMalop
+            // 
+            this.cbMalop.FormattingEnabled = true;
+            this.cbMalop.Location = new System.Drawing.Point(447, 152);
+            this.cbMalop.Name = "cbMalop";
+            this.cbMalop.Size = new System.Drawing.Size(145, 21);
+            this.cbMalop.TabIndex = 8;
+            this.cbMalop.Text = "Chọn";
+            // 
+            // ThemSinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 265);
+            this.Controls.Add(this.cbMalop);
+            this.Controls.Add(this.txtbirthday);
+            this.Controls.Add(this.rdbOthersexual);
+            this.Controls.Add(this.rdbFeMale);
+            this.Controls.Add(this.rdbMale);
+            this.Controls.Add(this.txtpod);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.txtfirstnam);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ThemSinhVien";
+            this.Text = "Thêm mới sinh viên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtfirstnam;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.TextBox txtpod;
+        private System.Windows.Forms.RadioButton rdbMale;
+        private System.Windows.Forms.RadioButton rdbFeMale;
+        private System.Windows.Forms.RadioButton rdbOthersexual;
+        private System.Windows.Forms.DateTimePicker txtbirthday;
+        private System.Windows.Forms.ComboBox cbMalop;
+    }
+}
---
+++ GUI/ThemSinhVien.cs
@@ -0,0 +1,129 @@
+﻿using AppQLSV.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppQLSV.GUI
+{
+    public partial class ThemSinhVien : Form
+    {
+        Student sinhvien;
+        public ThemSinhVien()
+        {
+            //thêm mới
+            InitializeComponent();
+            loadcombobox();
+            this.Text = "Thêm mới sinh viên";
+        }
+
+
+        public ThemSinhVien(Student sinhvien)
+        {
+            //chỉnh sửa
+            InitializeComponent();
+            loadcombobox();
+            this.sinhvien = sinhvien;
+            this.Text = "Chỉnh sửa sinh viên";
+            txtfirstnam.Text = this.sinhvien.FirstName;
+            txtlastname.Text = this.sinhvien.LastName;
+            this.sinhvien.DateOfBirth = txtbirthday.Value;
+            txtpod.Text = this.sinhvien.PlaceOfBirth;
+            cbMalop.Text = this.sinhvien.IDClassroom;
+            var gender = 0;
+            if (rdbMale.Checked == true)
+            {
+                gender = 0;
+            }
+            else if (rdbFeMale.Checked == true)
+            {
+                gender = 1;
+            }
+            else if (rdbOthersexual.Checked == true)
+            {
+                gender = 3;
+            }
+            this.sinhvien.Gender = gender;
+
+
+        }
+
+        void loadcombobox()
+        {
+            var db = new AppQLSVDBContext();
+            var cbbox = db.Classrooms.OrderBy(e => e.ID).ToList();
+            for (int i = 0; i < cbbox.Count; i++)
+            {
+                cbMalop.Items.Add(cbbox.ElementAt(i).ID);
+            }
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            {
+                var firstname = txtfirstnam.Text;
+                var lastname = txtlastname.Text;
+                var dob = txtbirthday.Value;
+                var pod = txtpod.Text;
+                var gender = 0;
+                var idclassrom = cbMalop.Text;
+                if (rdbMale.Checked == true)
+                {
+                    gender = 0;
+                }
+                else if (rdbFeMale.Checked == true)
+                {
+                    gender = 1;
+                }
+                else if (rdbOthersexual.Checked == true)
+                {
+                    gender = 3;
+                }
+
+                if (sinhvien == null)
+                {
+
+                    System.Random rd = new Random();
+                    var sv = new Student
+                    {
+                        ID = "T"+rd.Next(1,9999), //Guid.NewGuid().ToString(),
+                        FirstName = firstname,
+                        LastName = lastname,
+                        DateOfBirth = dob,
+                        PlaceOfBirth = pod,
+                        Gender = gender,
+                        IDClassroom = idclassrom
+                    };
+                    var db = new AppQLSVDBContext();
+                    db.Students.Add(sv);
+                    db.SaveChanges();
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    var db = new AppQLSVDBContext();
+                    var newsv = db.Students.Where(t => t.ID == sinhvien.ID).FirstOrDefault();
+                    newsv.FirstName = firstname;
+                    newsv.LastName = lastname;
+                    newsv.DateOfBirth = dob;
+                    newsv.PlaceOfBirth = pod;
+                    newsv.Gender = gender;
+                    newsv.IDClassroom = idclassrom;
+                    db.SaveChanges();
+                    DialogResult = DialogResult.OK;
+                }
+            }
+        }
+    }
+}
